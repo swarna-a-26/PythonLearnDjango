4133a0510918d812aa9e2d000fcc2aa89be06fa3
Swarna Ananthan <swarnaananthan@Swarnas-MBP-2.home>
2018-05-11 12:06:09 -0400

    Added django project

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/swarnaananthan/python-learn-django/PythonLearnDjango/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
